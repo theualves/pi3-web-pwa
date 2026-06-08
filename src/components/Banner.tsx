19390cd68bcd92dd3a981258f189dcb97945c1fb
Matheus Alves <matheusalves2906@gmail.com>
2026-06-08 15:37:43 -0300

    style: mudando cor do banner principal

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -14,7 +14,7 @@ const openSans = Open_Sans({
 
 export default function Banner() {
   return (
-    <section className="w-full bg-gradient-to-r from-[#92B800] via-[#A9D400] to-[#BCE600] overflow-hidden min-h-fit lg:min-h-[700px] flex">
+    <section className="w-full bg-gradient-to-r from-[#023787] to-[#1F76C3] overflow-hidden min-h-fit lg:min-h-[700px] flex">
       <div className="w-full max-w-[1400px] mx-auto px-6 md:px-12 flex flex-col lg:flex-row items-stretch">
         <div className="w-full lg:w-1/2 text-white flex flex-col justify-center items-center text-center lg:items-start lg:text-left py-12 lg:py-12 z-10">
           <Image
@@ -25,7 +25,7 @@ export default function Banner() {
           className="mb-6 w-[280px] lg:w-[450px] h-auto"
           />
           <p
-            className={`text-base font-semibold md:text-xl text-[#3F3F3F] mb-10 max-w-lg mx-auto lg:mx-0 ${openSans.className}`}
+            className={`text-base font-semibold md:text-xl text-[#EDEDED] mb-10 max-w-lg mx-auto lg:mx-0 ${openSans.className}`}
           >
             Um ambiente para envio, análise e validação de horas acadêmicas de
             forma ágil e segura.
