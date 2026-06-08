7c9515b1a544ecb963231faff5ec22d32c95999f
Matheus Alves <matheusalves2906@gmail.com>
2026-06-08 15:47:13 -0300

    style: afinamento no gradiente do banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -14,15 +14,15 @@ const openSans = Open_Sans({
 
 export default function Banner() {
   return (
-    <section className="w-full bg-gradient-to-r from-[#023787] to-[#1F76C3] overflow-hidden min-h-fit lg:min-h-[700px] flex">
+    <section className="w-full bg-gradient-to-r from-[#043573] to-[#1F76C3] overflow-hidden min-h-fit lg:min-h-[700px] flex">
       <div className="w-full max-w-[1400px] mx-auto px-6 md:px-12 flex flex-col lg:flex-row items-stretch">
         <div className="w-full lg:w-1/2 text-white flex flex-col justify-center items-center text-center lg:items-start lg:text-left py-12 lg:py-12 z-10">
           <Image
-          src="/horaMais.svg"
-          alt="logo Hora Mais - Senac"
-          width={800}
-          height={216}
-          className="mb-6 w-[280px] lg:w-[450px] h-auto"
+            src="/horaMais.svg"
+            alt="logo Hora Mais - Senac"
+            width={800}
+            height={216}
+            className="mb-6 w-[280px] lg:w-[450px] h-auto"
           />
           <p
             className={`text-base font-semibold md:text-xl text-[#EDEDED] mb-10 max-w-lg mx-auto lg:mx-0 ${openSans.className}`}
